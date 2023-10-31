2d2b10ab5ceccdbdf9f19378fb245eb3c5551b12
VanshJani <140586866+VanshJani@users.noreply.github.com>
2023-10-31 14:52:21 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    
-  }
+const nextConfig = {
+  reactStrictMode : true,
+  swcMinify : true,
+  output : "export"
+}
 
-  module.exports = nextConfig
+module.exports = nextConfig
