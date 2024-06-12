d44efa9d9ce27cb4aa832d3371328589af030895
Jani Vansh <vanshjani@mntechgroup.com>
2024-06-12 17:41:45 +0530

    userBio

--- pages/components/Maincomp/UserBio.js
+++ pages/components/Maincomp/UserBio.js
@@ -10,13 +10,13 @@ const ShowMore = ({ text, userid ,  maxLength = 100, className = '' }) => {
     router.push(`dashboard/${userid}`)
   };
 
-  const truncatedText = text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
+  const truncatedText = text?.length > maxLength ? text.slice(0, maxLength) + '...' : text;
 
   return (
     <div className={`mt-[20px] 2xl:mt-[20px] xl:mt-[15px] h-[45px] ${className}`}>
       <p className="text-[#979797] text-[14px] 2xl:text-[12px] xl:text-[12px] pr-[10px]">
         {showFullText ? text : truncatedText}
-        {text.length > maxLength && (
+        {text?.length > maxLength && (
            <span className="cursor-pointer underline text-[#0F52BA] cursor-pointer" onClick={toggleShowMore}>
             {showFullText ? ' less' : 'more'}
           </span>
